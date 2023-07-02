6404093d9a4f48f076bb770267b0712e885abe8d
Ludovic Vannoorenberghe <ludovic.vannoorenberghe@elephant.healthcare>
2023-07-02 12:00:02 +0100

    unit test for delete pod

---
+++ tests/test_pod_chaos_monkey.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from pod_chaos_monkey.namespaced_delete import delete_random_pod
+
+class TestPodChaosMonkey(unittest.TestCase):
+
+  @patch('pod_chaos_monkey.namespaced_delete.client.CoreV1Api')
+  @patch('pod_chaos_monkey.namespaced_delete.config')
+  def test_delete_random_pod(self, mock_config, mock_core_v1_api):
+    namespace = "target-namespace"
+
+    # Mock a pod
+    mock_pod = MagicMock()
+    mock_pod.metadata.name = "mock-pod"
+    # Mock the returned list of pods
+    mock_core_v1_api.return_value.list_namespaced_pod.return_value.items = [mock_pod]
+
+    delete_random_pod(namespace)
+    # Verify that the delete_namespaced_pod method is called
+    mock_core_v1_api.return_value.delete_namespaced_pod.assert_called_with("mock-pod", namespace)
+
+
+if __name__ == '__main__':
+  unittest.main()
